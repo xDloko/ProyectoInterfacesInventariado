b74155c8c4f5e6185791bcfbe42dafa037a9647d
xDloko <santiagopcuenca18@gmail.com>
2025-07-17 19:25:44 -0500

    AdminCreate Automatico para deploy

--- PyramidInsume/users/apps.py
+++ PyramidInsume/users/apps.py
@@ -1,9 +1,25 @@
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
     def ready(self):
-        import users.signals
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError, ProgrammingError
+        from django.core.management import call_command
+
+        User = get_user_model()
+
+        try:
+            if not User.objects.filter(email='admin@example.com').exists():
+                print("🛠️ Creando usuario administrador predeterminado...")
+                User.objects.create_superuser(
+                    email='admin@example.com',
+                    password='admin123',
+                    user_type=1,  # Administrador
+                    ciudad='BOG'
+                )
+        except (OperationalError, ProgrammingError):
+            # Evita errores cuando aún no se ha creado la tabla (por ejemplo, en migrate inicial)
+            pass
